683f5daddb790d6b494a6924861bec9072e00412
Darling91 <87092064+Darling91@users.noreply.github.com>
2025-12-23 10:05:02 +0300

    Add files via upload

---
+++ blog/src/test/java/src/pet/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package src.pet.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
